938235b0200c98b2062578da1f7a564f44d4f32c
Sonia Jimenez <112973252+Soniajim24@users.noreply.github.com>
2023-03-26 19:51:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './home';
+import Home from './Home';
 import NavBar from './NavBar';
-import CreateAccount from './createaccount';
-import Deposit from './deposit';
-import Withdraw from './withdraw';
-import AllData from './alldata';
+import CreateAccount from './CreateAccount';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import AllData from './AllData';
 
-function App() {
+export default function App() {
+
   return (
     <>
-    <Router>
+      <Router>
         <NavBar />
         <div className="container">
-       <Routes>
+          <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/createAccount" element={<CreateAccount />}  />
-            <Route path="/deposit" element={<Deposit/>} />
-            <Route path="/withdraw" element={<Withdraw/>} />
-            <Route path="/allData" element={<AllData/>} />
-            </Routes>
+            <Route
+              path="/createAccount"
+              element={
+                <>
+                <CreateAccount
+                />
+                </>
+              }
+            />
+            <Route
+              path="/deposit"
+              element={<Deposit />}
+            />
+            <Route
+              path="/withdraw"
+              element={<Withdraw />}
+            />
+            <Route
+              path="/allData"
+              element={<AllData />}
+            />
+          </Routes>
         </div>
-    </Router>
-    </>
-  );
-}
-
-export default App;
+      </Router>
+      </>
+  )};
+    
